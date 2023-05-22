3a1901f1d6ade5aa054a6295ab25db41eb66a2e1
ianabreu <ianabreudev@gmail.com>
2023-05-22 04:56:58 -0300

    Editando e deletando casas

--- src/routes.js
+++ src/routes.js
@@ -8,8 +8,11 @@ import HouseController from './controllers/HouseController';
 const routes = new Router();
 const upload = multer(uploadConfig);
 
-routes.post('/sessions', SessionController.store)
+routes.post('/sessions', SessionController.store);
 
-routes.post('/houses', upload.single('thumbnail'), HouseController.store)
+routes.post('/houses', upload.single('thumbnail'), HouseController.store);
+routes.get('/houses', HouseController.index);
+routes.put('/houses/:house_id', upload.single('thumbnail'), HouseController.update);
+routes.delete('/houses/', HouseController.destroy);
 
 export default routes;
