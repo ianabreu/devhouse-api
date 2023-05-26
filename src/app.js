1be7756556b666695fa730e9c71299baef0af4c4
ianabreu <ianabreudev@gmail.com>
2023-05-26 03:36:57 -0300

    instalando cors

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import path from 'path';
+import cors from 'cors'
 import 'dotenv/config';
 
 import routes from './routes';
 
 
-class App{
+class App {
 
-    constructor(){
+    constructor() {
         this.server = express();
 
         mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@devhouse.7f1gkqm.mongodb.net/devhouse?retryWrites=true&w=majority`, {
@@ -19,8 +20,13 @@ class App{
         this.routes();
     }
     middlewares() {
+        this.server.use(cors({}));
+
+        this.server.use(
+            '/files',
+            express.static(path.resolve(__dirname, '..', 'uploads'))
+        );
         this.server.use(express.json());
-        this.server.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
     }
     routes() {
         this.server.use(routes);
