9dc475a8c124daedcdc8dba6992447a6f52ba739
ianabreu <ianabreudev@gmail.com>
2023-05-26 05:58:04 -0300

    Adicionando validações de email do SessionControllerr

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -5,13 +5,22 @@ update: Alterar alguma sessao,
 store: Criar uma sessao, 
 destroy: Deletar uma sessao,
 */
+import * as Yup from 'yup';
 import User from '../models/User';
 
 
 class SessionController {
     async store(req, res) {
+        const schema = Yup.object().shape({
+            email: Yup.string().email().required(),
+        });
+
         const { email } = req.body;
         let user = await User.findOne({ email })
+
+        if(!(await schema.isValid(req.body))) {
+            return res.status(400).json({ error: "Falha na validação" })
+        } 
 
         if (!user) {
             user = await User.create({ email })
